6b23a2e0a1c2072e320d9eb3acf7193ac0de08d5
Maya Rosenbaum <mayarosenbaum@ucsb.edu>
2024-10-09 12:16:14 -0700

    xy - updated with actual developer name

--- src/test/java/edu/ucsb/cs156/spring/hello/DeveloperTest.java
+++ src/test/java/edu/ucsb/cs156/spring/hello/DeveloperTest.java
@@ -22,12 +22,23 @@ public class DeveloperTest {
 
     @Test
     public void getName_returns_correct_name() {
-        // TODO: Replace Chris G. with your name as shown on
-        // <https://bit.ly/cs156-f24-teams>
-        assertEquals("Chris G.", Developer.getName());
+        assertEquals("Maya", Developer.getName());
     }
 
+    /*
     // TODO: Add additional tests as needed to get to 100% jacoco line coverage, and
+    public void getTeam_returns_correct_team() {
+        // TODO: Replace Chris G. with your name as shown on
+        // <https://bit.ly/cs156-f24-teams>
+        assertEquals("Maya", Developer.team());
+    }
+
+    public void getGithubId_returns_correct_githubId() {
+        // TODO: Replace Chris G. with your name as shown on
+        // <https://bit.ly/cs156-f24-teams>
+        assertEquals("Maya", Developer.getName());
+    }
+    */
     // 100% mutation coverage (all mutants timed out or killed)
 
 }
